1c107b92e0e06c4afaf5851513cbb81c515c4d8c
zebrashead <igor.scour@list.ru>
2020-02-10 01:58:16 +0300

    Remove email password

--- btre/settings.py
+++ btre/settings.py
@@ -147,8 +147,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'igorbilenko2106@gmail.com'
-EMAIL_HOST_PASSWORD = '37283468igoR'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
